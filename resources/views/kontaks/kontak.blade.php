d0abe3016fa1a8551bc598b5af30700f2d90e831
new name <rahmada365@gmail.com>
2023-05-25 00:28:00 +0700

    create kontak css, bg bg2, kontak.

--- resources/views/kontaks/kontak.blade.php
+++ resources/views/kontaks/kontak.blade.php
@@ -25,12 +25,14 @@
 
 <body>
 
-<section>
-        <h5>kontak</h5>
-    </section>
+<div class="bg">
+<img src="pic/bg.jpg" alt="">
+</div>
+
+
 
 <section>
-    <div class="peta">
+    <div class="container">
     <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m11!1m3!1d153.16494488733247!2d114.36953796912474!3d-8.221767725017516!2m2!1f288.1985355620295!2f45!3m2!1i1024!2i768!4f35!3m3!1m2!1s0x2dd14532caf42b99%3A0x14eb77fb0fb2c86b!2sDewan%20Kesenian%20Blambangan!5e1!3m2!1sid!2sid!4v1684834286645!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 
@@ -41,25 +43,25 @@
     <!-- icons -->
     <div class="icons">
         <div class="telp">
-        <div class="card mb-3" style="width: 18rem;">
+        <div class="card mb-3 " style="width: 18rem;">
   <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
+    <h5 class="card-title">facebook</h5>
     <a href="https://www.facebook.com/dkbbwi2020"> <i class="bx bxl-facebook-circle"> </i></a>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <a href="https://www.facebook.com/dkbbwi2020" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
 
-<div class="card text-center mb-3" style="width: 18rem;">
+<div class="card text-center mb-3 " style="width: 18rem;">
   <div class="card-body ">
-    <h5 class="card-title">Special title treatment</h5>
+    <h5 class="card-title">WhatsaApp</h5>
     <a href="https://www.youtube.com/@dewankesenianblambangan488"><i class='bx bxs-phone'></i> </a>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <a href="https://www.youtube.com/@dewankesenianblambangan488" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
 
-<div class="card text-end" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
+<div class="card text-center mb-3 " style="width: 18rem;">
+  <div class="card-body ">
+    <h5 class="card-title">Email</h5>
     <a href="https://www.youtube.com/@dewankesenianblambangan488"><i class='bx bxs-envelope'></i> </a>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
@@ -70,6 +72,13 @@
 
         </div>
     </div>
+</section>
+
+
+<section>
+  <div class="variasi">
+    <!--  -->
+  </div>
 </section>
 
 
